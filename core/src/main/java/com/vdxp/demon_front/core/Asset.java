03c361dcfcb725170c58eb7f48ba9a0d8a3d2cee
wrongcoder <git@wrongcoder.com>
2013-07-23 18:07:34 -0700

    title splash

--- core/src/main/java/com/vdxp/demon_front/core/Asset.java
+++ core/src/main/java/com/vdxp/demon_front/core/Asset.java
@@ -8,6 +8,9 @@ import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 public class Asset {
 
 	public static final String mono16Font = "fonts/liberation-mono-16.fnt";
+	public static final String sans16Font = "fonts/liberation-sans-16.fnt";
+	public static final String sans24Font = "fonts/liberation-sans-24.fnt";
+	public static final String sans48Font = "fonts/liberation-sans-48.fnt";
 
 	public static final String loadingAtlas = "textures/loading.atlas";
 	public static final String loadingText = "text";
@@ -24,6 +27,9 @@ public class Asset {
 		// Remaining assets
 		assetManager.load(libgdxLogo, Texture.class);
 		assetManager.load(mono16Font, BitmapFont.class);
+		assetManager.load(sans16Font, BitmapFont.class);
+		assetManager.load(sans24Font, BitmapFont.class);
+		assetManager.load(sans48Font, BitmapFont.class);
 		assetManager.load(spritesAtlas, TextureAtlas.class);
 	}
 
--- core/src/main/java/com/vdxp/demon_front/core/Util.java
+++ core/src/main/java/com/vdxp/demon_front/core/Util.java
@@ -1,5 +1,8 @@
 package com.vdxp.demon_front.core;
 
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
 /** static members only! */
 public class Util {
 
@@ -7,4 +10,9 @@ public class Util {
 		return (prev * (1 - alpha)) + (curr * alpha);
 	}
 
+	static void drawCentred(final String text, final BitmapFont font, final SpriteBatch batch, final float y) {
+		final BitmapFont.TextBounds bounds = font.getBounds(text);
+		final float centreX = (Game.WIDTH - bounds.width) / 2;
+		font.draw(batch, text, centreX, y);
+	}
 }
